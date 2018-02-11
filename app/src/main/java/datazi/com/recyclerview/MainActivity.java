c12874ce417e4c6ee4e0b1b3f2eed63e997b7f24
Ashish <ashishkvs@gmail.com>
2018-02-11 12:04:11 +0530

    intial commit

---
+++ app/src/main/java/datazi/com/recyclerview/MainActivity.java
@@ -0,0 +1,99 @@
+package datazi.com.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ProductAdapter adapter;
+    List<Product> productList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //recyclerv view
+        productList=new ArrayList<>();
+        recyclerView=findViewById(R.id.recyclerView);
+        //set the size of recycler view
+        recyclerView.setHasFixedSize(true);
+
+        //layout vertical by default we can set grid
+        recyclerView.setLayoutManager(new LinearLayoutManager((this)));
+
+        //add item to the list
+        productList.add(
+                new Product(
+                        1,
+                        "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.coir4));
+
+        productList.add(
+                new Product(
+                        1,
+                        "Dell Inspiron 7000 Core i5 7th Gen - (8 GB/1 TB HDD/Windows 10 Home)",
+                        "14 inch, Gray, 1.659 kg",
+                        4.3,
+                        60000,
+                        R.drawable.coir2));
+
+        productList.add(
+                new Product(
+                        1,
+                        "Microsoft Surface Pro 4 Core m3 6th Gen - (4 GB/128 GB SSD/Windows 10)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.coir3));
+        //adapter
+        adapter=new ProductAdapter(this,productList);
+        recyclerView.setAdapter(adapter);
+
+        //edit Text-------------------
+        EditText editText=findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //user define method
+                filter(editable.toString());
+
+            }
+        });
+
+    }
+    private void filter(String text){
+        ArrayList<Product> filteredList=new ArrayList<>();
+
+        for(Product item :productList){
+            if(item.getTitle().toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(item);
+            }
+        }
+
+        adapter.filterList(filteredList);
+
+    }
+}
---
+++ app/src/main/java/datazi/com/recyclerview/ProductAdapter.java
@@ -0,0 +1,102 @@
+package datazi.com.recyclerview;
+
+/**
+ * Created by Ashish on 2/11/2018.
+ */
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * RecyclerView.Adapter
+ * RecyclerView.ViewHolder
+ */
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    private Context context;  //in order to bind xml layout inflater we need context
+    private List<Product> productList;
+    private RelativeLayout relativeLayout;
+
+    public ProductAdapter(Context context, List<Product> productList) {
+        this.context = context;
+        this.productList = productList;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View view=inflater.inflate(R.layout.recycler_list,null);
+        ProductViewHolder holder=new ProductViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        //bind data from LIST to Ui
+        Product product=productList.get(position);
+
+        //for 1st list object add all ui attributes
+        holder.textViewTitle.setText(product.getTitle());
+        holder.textViewDesc.setText(product.getShortdesc());
+        holder.textViewRating.setText(String.valueOf(product.getRating()));
+        holder.textViewPrice.setText(String.valueOf(product.getPrice()));
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"you clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //for image we need context obj inorder to fetch from drawable folder
+        holder.imageView.setImageDrawable(context.getResources().getDrawable(product.getImage()));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        //size of list
+        return productList.size();
+    }
+
+    /**
+     * for text change listener FIlter List
+     * @param filteredList
+     * pass new array list and update d sam to existing arraylist
+     */
+    public  void filterList(ArrayList<Product> filteredList){
+
+        productList=filteredList;
+        notifyDataSetChanged();
+    }
+
+    //INTERNAL CLASS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    public class ProductViewHolder extends RecyclerView.ViewHolder{
+        //ui element in order to bind with layout
+        ImageView imageView;
+        TextView textViewTitle,textViewDesc,textViewRating,textViewPrice;
+        RelativeLayout relativeLayout;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+            imageView=itemView.findViewById(R.id.imageView);
+            textViewDesc=itemView.findViewById(R.id.textViewShortDesc);
+            textViewPrice=itemView.findViewById(R.id.textViewPrice);
+            textViewRating=itemView.findViewById(R.id.textViewRating);
+            textViewTitle=itemView.findViewById(R.id.textViewTitle);
+            relativeLayout=itemView.findViewById(R.id.relative_layout);
+        }
+    }
+}
